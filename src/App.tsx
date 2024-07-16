ca5cc02b1012f5bb46d4f5e799a51f181f6ac3a2
raniagh <rghzaiel@ggn-group.com>
2024-07-16 11:28:25 +0100

    context API

--- src/App.tsx
+++ src/App.tsx
@@ -14,83 +14,18 @@ function App() {
 }
 export default App;*/
 
-import { useReducer } from 'react';
-import { authenticate, User } from './api/authenticate';
 import { UserHeader } from './UserHeader';
 import { Main } from './Main';
-import { authorize } from './api/authorize';
+import { AppProvider } from './AppContext';
 
-type State = {
-  user: undefined | User;
-  permissions: undefined | string[];
-  loading: boolean;
-};
-const initialState: State = {
-  user: undefined,
-  permissions: undefined,
-  loading: false,
-};
-
-//Different type of actions that can update the state
-type Action =
-  | {
-      type: 'authenticate';
-    }
-  | {
-      type: 'authenticated';
-      user: User | undefined;
-    }
-  | {
-      type: 'authorize';
-    }
-  | {
-      type: 'authorized';
-      permissions: string[];
-    };
-
-function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case 'authenticate':
-      return { ...state, loading: true };
-    case 'authenticated':
-      return { ...state, loading: false, user: action.user };
-    case 'authorize':
-      return { ...state, loading: true };
-    case 'authorized':
-      return {
-        ...state,
-        loading: false,
-        permissions: action.permissions,
-      };
-    default:
-      return state;
-  }
-}
-
+//Add the AppProvider so the components can access the context
 function App() {
-  const [{ user, permissions, loading }, dispatch] = useReducer(reducer, initialState);
-
-  async function handleSignInClick() {
-    dispatch({ type: 'authenticate' });
-    const authenticatedUser = await authenticate();
-    dispatch({
-      type: 'authenticated',
-      user: authenticatedUser,
-    });
-    if (authenticatedUser !== undefined) {
-      dispatch({ type: 'authorize' });
-      const authorizedPermissions = await authorize(authenticatedUser.id);
-      dispatch({
-        type: 'authorized',
-        permissions: authorizedPermissions,
-      });
-    }
-  }
-
   return (
     <div className="max-w-7xl mx-auto px-4">
-      <UserHeader user={user} onSignInClick={handleSignInClick} loading={loading} />
-      <Main user={user} permissions={permissions} />
+      <AppProvider>
+        <UserHeader />
+        <Main />
+      </AppProvider>
     </div>
   );
 }
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,17 +1,14 @@
-import { User } from './api/authenticate';
+import { useAppContext } from './AppContext';
 import { Content } from './Content';
 
-type Props = {
-  user: undefined | User;
-  permissions: undefined | string[];
-};
+export function Main() {
+  const { user } = useAppContext();
 
-export function Main({ user, permissions }: Props) {
   return (
     <main className="py-8">
       <h1 className="text-3xl text-center font-bold underline">Welcome</h1>
       <p className="mt-8 text-xl text-center">{user ? `Hello ${user.name}!` : 'Please sign in'}</p>
-      <Content permissions={permissions} />
+      <Content />
     </main>
   );
 }
